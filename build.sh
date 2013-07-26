df7111b61e428faa4a0f884a3dc2e6380d544f20
kevinneu <693512779@qq.com>
2013-07-26 14:55:36 +0800

    Init Version

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -x
+
+SOURCE_DIR=`pwd`
+BUILD_PLATFORM=${BUILD_PLATFORM:-LINUX}
+BUILD_DIR=${BUILD_DIR:-./build}
+
+mkdir -p $BUILD_DIR/$BUILD_PLATFORM \
+	&& cd $BUILD_DIR/$BUILD_PLATFORM \
+	&& cmake --graphviz=dep.dot \
+	$SOURCE_DIR \
+	&& make $*
